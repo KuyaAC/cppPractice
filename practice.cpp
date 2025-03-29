c890efb76d10cf7559178bfda4571452706afc05
KuyaAC <aciesnet02@gmail.com>
2025-03-29 21:48:01 +0800

    keep on practicing

--- practice.cpp
+++ practice.cpp
@@ -15,3 +15,21 @@ int main() {
   double MoM = (month2 - month1) / month1 * 100;
   std::cout << "\nThe MoM change is " << MoM << "%";
 }
+
+// Cat Years 🐈
+
+#include <iostream>
+
+int main() {
+  int catAge;
+  int humanYears;
+
+  std::cout << "Welcome to the Cat Years program! This only works for cats 3+ years old.\n\n";
+  
+  std::cout << "Enter your cat's age: ";
+  std::cin >> catAge;
+
+  humanYears = (catAge - 2) * 4 + 24;
+
+  std::cout << "Your cat is " << humanYears << " years old in human years.\n";
+}
