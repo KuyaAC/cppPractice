4935df9ce2383f81232e8cfa72a8327e910a2f26
KuyaAC <aciesnet02@gmail.com>
2025-03-31 21:36:38 +0800

    practice sql a bit too

---
+++ practice.sql
@@ -0,0 +1,8 @@
+-- Show all the DB from shows
+SELECT * FROM shows; 
+-- For specific Columns
+SELECT id, name, genre
+FROM shows;
+-- DISTINCT for uniqueness walang paulit ulit
+SELECT DISTINCT genre
+FROM shows;
