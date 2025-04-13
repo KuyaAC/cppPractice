5a6e4a075b847f505a49f77dc80b3d4b827044a5
KuyaAC <aciesnet02@gmail.com>
2025-04-13 21:27:57 +0800

    diff data types

--- practice.java
+++ practice.java
@@ -20,4 +20,20 @@ public class HelloWorld {
     }
 }
 
+// Using different datatypes
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Ellie";
+        String state = "Illinois";
+        char gender = 'F';
+        int age = 28;
+        boolean organDonor = false;
+
+        System.out.println(name);
+        System.out.println(state);
+        System.out.println(gender);
+        System.out.println(age);
+        System.out.println(organDonor);
+    }
+}
 
