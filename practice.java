105b97cc144dedf55453a928a2dbe85bf4a07e80
KuyaAC <aciesnet02@gmail.com>
2025-04-13 21:19:11 +0800

    Int practice

--- practice.java
+++ practice.java
@@ -5,3 +5,19 @@ public class HelloWorld{
   }
 }
 
+//int variable sample
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        int livingRoom = 15;
+        int masterBedRoom = 45;
+        int bathRoom = 10;
+
+        System.out.println("The living Room has " + (livingRoom) + " items"); 
+        System.out.println("The Master Bedroom has " + (masterBedRoom) + " items"); 
+        System.out.println("The Bathroom has " + (bathRoom) + " items");  
+        
+    }
+}
+
+
