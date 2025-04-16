d66061bca10f06cd6965e57f638de06bad41dae8
KuyaAC <aciesnet02@gmail.com>
2025-04-16 15:50:47 +0800

    if statement

--- practice.java
+++ practice.java
@@ -92,4 +92,17 @@ public class HelloWorld {
     }
 }
 
+// if statement:
+
+// Paste code below 💖
+public class HelloWorld {
+    public static void main(String[] args) {
+        int oats = 2;
+
+        if (oats > 0) {
+            System.out.println("Let's make oatmeal today!");
+        } 
+    }
+}
+
 
