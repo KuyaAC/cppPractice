5eadb21ea411b3c1008dbf6033e837c91254bdb3
KuyaAC <aciesnet02@gmail.com>
2025-04-17 16:19:29 +0800

    Practice Boolean and conditional statement

--- practice.java
+++ practice.java
@@ -105,4 +105,20 @@ public class HelloWorld {
     }
 }
 
+//Practice Boolean
+public class HelloWorld {
+    public static void main(String[] args) {
+        boolean isCatOwner = false;
+
+        if (isCatOwner == true){
+            System.out.println("20% off selected cat items with code MEOW2025");
+        }
+        else{
+            System.out.println("Welcome to the Pets Pets Store!");
+        }
+        
+    }
+}
+
+
 
