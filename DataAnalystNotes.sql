6b46fb90612a1bc55c2756d3a4744de5c7b9606b
KuyaAC <aciesnet02@gmail.com>
2025-05-20 12:37:35 +0800

    practice DA

--- DataAnalystNotes.sql
+++ DataAnalystNotes.sql
@@ -7,6 +7,21 @@ SELECT COUNT (*) AS total_records
 FROM people;
 -- OUTPUT: Total count total records
 
+SELECT COUNT(birthdate) AS count_birthdate -- Count the number of birthdates in the people table
+FROM people;
+
+SELECT COUNT(language) AS count_language, COUNT(country) AS count_countries
+-- Count the records for languages and countries represented in the films table
+FROM films;
+
 -- DISTINT (Remove duplicates to return only unique value)
 SELECT DISTINCT language
 FROM films;
+
+-- COUNT DISTINCT
+SELECT COUNT(DISTINCT country) AS count_distinct_countries
+FROM films;
+
+
+
+
