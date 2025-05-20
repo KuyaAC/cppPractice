506304a757836e716341ad5cc8c30feb25150d43
KuyaAC <aciesnet02@gmail.com>
2025-05-20 11:28:50 +0800

    distinct

--- DataAnalystNotes.sql
+++ DataAnalystNotes.sql
@@ -1,4 +1,12 @@
 -- LETSS GOOO!!
-SELECT COUNT birthdate AS count_birthdates
+SELECT COUNT (birthdate) AS count_birthdates
 FROM people;
 -- OUTPUT: Total count of birthdates from people table
+
+SELECT COUNT (*) AS total_records 
+FROM people;
+-- OUTPUT: Total count total records
+
+-- DISTINT (Remove duplicates to return only unique value)
+SELECT DISTINCT language
+FROM films;
