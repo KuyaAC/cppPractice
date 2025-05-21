c95ea614345808b4c9e2830deea65d406eb82758
KuyaAC <aciesnet02@gmail.com>
2025-05-21 21:43:31 +0800

    finish day2 datacamp

--- DataAnalystNotes.sql
+++ DataAnalystNotes.sql
@@ -99,6 +99,141 @@ FROM films
 WHERE (language = 'German')
     AND (release_year > 2000 AND release_year < 2010);
 
+-- Find the title and year of films from the 1990 or 1999
+SELECT title, release_year
+FROM films 
+WHERE release_year = 1990 
+    OR release_year = 1999;
+
+-- Add a filter to see only English or Spanish-language films
+SELECT title, release_year
+FROM films
+WHERE (release_year = 1990 OR release_year = 1999) 
+AND (language = 'English' OR language = 'Spanish');
+
+-- Filter films with more than $2,000,000 gross
+SELECT title, release_year
+FROM films
+WHERE (release_year = 1990 OR release_year = 1999)
+AND (language = 'English' OR language = 'Spanish')
+AND (gross > 2000000);
+
+-- ACT USING BETWEEN:
+
+-- Select the title and release_year for films released between 1990 and 2000
+SELECT title, release_year
+FROM films
+WHERE release_year
+    BETWEEN 1990 AND 2000;
+
+-- Narrow down your query to films with budgets > $100 million
+SELECT title, release_year
+FROM films
+WHERE (release_year BETWEEN 1990 AND 2000)
+    AND (budget > 100000000);
+
+-- Restrict the query to only Spanish-language films
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+	AND budget > 100000000
+	AND language = 'Spanish';
+
+-- Amend the query to include Spanish or French-language films
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+	AND budget > 100000000
+	AND (language = 'Spanish' OR language = 'French');
+
+-- LIKE OPERATOR
+SELECT name 
+FROM people
+WHERE name LIKE 'Ade%';
+-- Matches the name that start with 'Ade'
+-- '%' match 0-Many characters
+-- '_' match a single chatacter
+
+-- NOT LIKE OPERATOR:
+SELECT name
+FROM people 
+WHERE name NOT LIKE 'A.%';
+-- find record for people who are not have 'A.' in the first name (CASE SENSITIVE)
+
+-- WILD CARD CAN POSITION EVERYWHERE
+SELECT name 
+FROM people
+WHERE name LIKE '%r';
+-- Find records where name ends with 'r'
+
+SELECT name 
+FROM people
+WHERE name LIKE '__t%';
+-- Finds record where third character is 't'
+
+-- IN operator:
+SELECT title 
+FROM films
+WHERE release_year IN (1920, 1930, 1940);
+-- make it easier to set numerous OR conditions
+
+SELECT title
+FROM films
+WHERE country IN ('Germany', 'France');
+-- Output title where country is associated either Germany or France
+
+-- ACT (LIKE and NOT lIKE):
+
+-- Select the names that start with B
+SELECT name
+FROM people
+WHERE name LIKE 'B%';
+
+-- Select the names that have r as the second letter
+SELECT name
+FROM people
+WHERE name LIKE '_r%';
+
+-- Select names that don't start with A
+SELECT name
+FROM people
+WHERE name NOT LIKE 'A%';
+
+-- ACT (WHERE IN):
+-- Find the title and release_year for all films over two hours in length released in 1990 and 2000
+SELECT title, release_year
+FROM films
+WHERE duration > 120 
+    AND release_year IN (1990, 2000);
+
+-- Find the title and language of all films in English, Spanish, and French
+SELECT title, language
+FROM films
+WHERE language IN ('English', 'Spanish', 'French');
+
+-- Find the title, certification, and language all films certified NC-17 or R that are in English, Italian, or Greek
+SELECT title, certification, language
+FROM films
+WHERE certification IN ('NC-17', 'R')
+    AND language IN ('English', 'Greek', 'Italian');
+
+-- Count the unique titles
+SELECT COUNT(DISTINCT title) AS nineties_english_films_for_teens
+FROM films
+-- Filter to release_years to between 1990 and 1999
+WHERE (release_year
+	BETWEEN 1990 AND 1999)
+-- Filter to English-language films
+	AND (language = 'English')
+-- Narrow it down to G, PG, and PG-13 certifications
+	AND certification IN ('G', 'PG', 'PG-13');
+
+
+
+
+
+
+
 
 
 
