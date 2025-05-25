7815c72a64559a240cf0ab3cd8f7adbb5a1fb219
KuyaAC <aciesnet02@gmail.com>
2025-05-25 17:17:26 +0800

    WHERE and AGGRE

--- DataAnalystNotes.sql
+++ DataAnalystNotes.sql
@@ -265,6 +265,33 @@ FROM films;
 SELECT MIN(duration) AS shortest_film
 FROm films;
 
+-- USING WHERE WITH AGGREGATE FUNCTIONS:
+
+-- ROUND(number_to_round, decimal_places)
+SELECT ROUND(AVG(budget), 2) AS avg_budget
+FROM films
+WHERE release_year = 2010;
+
+-- Calculate the sum of gross from the year 2000 or later
+SELECT SUM(gross) AS total_gross
+FROM films
+WHERE release_year >= 2000;
+
+-- Calculate the average gross of films that start with A
+SELECT AVG(gross) AS avg_gross_A
+FROM films
+WHERE title LIKE 'A%';
+
+-- Calculate the lowest gross film in 1994
+SELECT MIN(gross) AS lowest_gross
+FROM films
+WHERE release_year = 1994;
+
+-- Calculate the highest gross film released between 2000-2012
+SELECT MAX(gross) AS highest_gross
+FROM films
+WHERE release_year BETWEEN 2000 AND 2012;
+
 
 
 
