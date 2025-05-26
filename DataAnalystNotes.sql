f4342dfcefdbcfdbc6e1082b01e6cf7f35cc75c0
KuyaAC <aciesnet02@gmail.com>
2025-05-26 18:26:14 +0800

    Practice GROUP BY

--- DataAnalystNotes.sql
+++ DataAnalystNotes.sql
@@ -351,6 +351,32 @@ SELECT title, duration
 FROM films
 ORDER BY duration DESC;
 
+-- GROUP BY:
+SELECT certification, COUNT(title) AS title_count
+FROM films
+GROUP BY certification;
+
+SELECT certification, COUNT(title) AS title_count
+FROM films
+GROUP BY certification
+ORDER BY title_count DESC;
+
+-- ACT:
+-- Find the release_year and film_count of each year
+SELECT release_year, COUNT(title) AS film_count
+FROM films
+GROUP BY release_year;
+
+-- Find the release_year and average duration of films for each year
+SELECT release_year, AVG(duration) AS avg_duration
+FROM films
+GROUP BY release_year;
+
+-- Find the release_year, country, and max_budget, then group and order by release_year and country
+SELECT release_year, country, MAX(budget) AS max_budget
+FROM films
+GROUP BY release_year, country;
+
 
 
 
