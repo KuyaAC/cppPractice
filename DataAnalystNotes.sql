a885faf4ee3950691b19e78d4d585e48f337354f
KuyaAC <aciesnet02@gmail.com>
2025-05-30 10:31:07 +0800

    2 joints

--- DataAnalystNotes.sql
+++ DataAnalystNotes.sql
@@ -516,6 +516,17 @@ INNER JOIN economies AS e
 -- Match on country code
 USING(code);
 
+SELECT name, e.year, fertility_rate, unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+INNER JOIN economies AS e
+ON c.code = e.code
+-- Add an additional joining condition such that you are also joining on year
+		AND p.year = e.year;
+
+
+
 
 
 
