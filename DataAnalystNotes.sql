18aff1e6b6ac8166ed359e260aaa667e4813e423
KuyaAC <aciesnet02@gmail.com>
2025-06-03 15:59:25 +0800

    self join done

--- DataAnalystNotes.sql
+++ DataAnalystNotes.sql
@@ -714,6 +714,24 @@ ON p1.continent = p2.continent
     AND p1.country <> p2.country
 LIMIT 10;
 
+-- ACT:
+-- Select aliased fields from populations as p1
+SELECT p1.country_code, p1.size AS size2010, p2.size AS size2015
+-- Join populations as p1 to itself, alias as p2, on country code
+FROM populations AS p1
+INNER JOIN populations AS p2
+USING(country_code);
+
+SELECT 
+	p1.country_code, 
+    p1.size AS size2010, 
+    p2.size AS size2015
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code
+WHERE p1.year = 2010
+-- Filter such that p1.year is always five years before p2.year
+    AND p2. year = (p1.year + 5)
 
 
 
