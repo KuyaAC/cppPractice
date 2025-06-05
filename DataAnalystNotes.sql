f1ec33fbccc737121f2ec323483e2d9d7262a73b
KuyaAC <aciesnet02@gmail.com>
2025-06-05 10:33:35 +0800

    UNION ALLL

--- DataAnalystNotes.sql
+++ DataAnalystNotes.sql
@@ -771,6 +771,21 @@ SELECT *
 FROM economies2019
 ORDER BY code, year;
 
+-- Query that determines all pairs of code and year from economies and populations, without duplicates
+SELECT code, year
+FROM economies
+UNION
+SELECT country_code, year
+FROM populations;
+
+-- WITH ALL POSSIBLE COMBINATION(with duplicates)
+SELECT code, year
+FROM economies
+-- Set theory clause
+UNION ALL
+SELECT country_code, year
+FROM populations
+ORDER BY code, year;
 
 
 
